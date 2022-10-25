68d9c02ce859a7292b1e2df0ab9575c38f839bf4
Chandan Kumar <chandan181singh@gmail.com>
2022-10-25 22:23:25 +0530

    Hello

---
+++ src/Swing/Calculator.java
@@ -0,0 +1,12 @@
+package Swing;
+
+import javax.swing.*;
+
+public class Calculator {
+    private JTextArea addTextArea;
+    private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
